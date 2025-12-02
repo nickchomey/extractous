293ad0c04140044e189f8656156e6e676f2ba2d7
Nick Chomey <nick_chomey@hotmail.com>
2025-12-02 15:10:34 -0600

    increase max size for string extraction

--- extractous-core/examples/extract_to_string.rs
+++ extractous-core/examples/extract_to_string.rs
@@ -6,7 +6,7 @@ fn main() {
     let file_path = &args[1];
 
     // Extract the provided file content to a string
-    let extractor = Extractor::new().set_xml_output(true);
+    let extractor = Extractor::new().set_xml_output(true).set_extract_string_max_length(10_000_000); // Set to 10MB or whatever you need;
     let (content, _metadata) = extractor.extract_file_to_string(file_path).unwrap();
     println!("{}", content);
 }
